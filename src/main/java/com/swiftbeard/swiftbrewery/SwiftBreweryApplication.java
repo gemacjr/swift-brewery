f7c279a568e3d00473c76b752009bc86fb6f4106
Ed <edmccormic@gmail.com>
2020-03-13 09:53:00 -0400

    init

---
+++ src/main/java/com/swiftbeard/swiftbrewery/SwiftBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.swiftbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swiftbeard/swiftbrewery/SwiftBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.swiftbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
